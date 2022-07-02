3b37578c957ad0eb45038f2f5deb7771a44e37dc
Uncle_Iroh <vadegtyarev@gmail.com>
2022-07-02 17:37:22 +0500

    Решил задачу 19

---
+++ Task 19/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет,
+//  является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.Clear();
+Console.Write("Чтобы узнать является ли число полиномом, введите пятизначное целое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num < 10000 || num > 99999)
+{
+    Console.WriteLine("Введенное Вами число не пятизначное!");
+    return;
+}
+
+int[] array = new int[5];
+int index = 0;
+while (index <= 4)
+{
+    array[index] = num % 10;
+    num = (num - array[index]) / 10;
+    index++;
+}
+if (array[0] == array[4] && array[1] == array[3])
+{
+    Console.WriteLine("Да! Введенное Вами число является палиндромом!");
+}
+else
+{
+    Console.WriteLine("Нет! Введенное Вами число является не палиндромом!");
+}
