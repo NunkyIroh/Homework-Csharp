765bb3d232d8ec684039d8cf06b58882176e0ede
Uncle_Iroh <vadegtyarev@gmail.com>
2022-07-15 21:56:05 +0300

    Решил задачу 52

---
+++ Task 52/Program.cs
@@ -0,0 +1,106 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+// for (int i = 0; i < N; i++)
+// {
+//     for (int j = 0; j < M; j++)
+//     {
+//         Console.Write(matrix[i, j]); //вывод матрицы
+//         summ[i] += matrix[j, i];      //подсчет суммы колонки
+//     } 
+//     Console.WriteLine();
+// }
+
+// Console.WriteLine();
+
+// foreach (double elem in summ)
+// {
+//     Console.WriteLine(elem/M); //вывод среднего для колонки
+// }
+
+Console.Clear();
+
+Console.WriteLine("Чтобы создать массив следуйте инструкциям!");
+
+Console.Write("Введите количество строк массива: ");
+int m = int.Parse(Console.ReadLine());
+
+Console.Write("Введите количество столбцов массива: ");
+int n = int.Parse(Console.ReadLine());
+
+Console.Write("Введите минимальное значение элементов массива: ");
+int min = int.Parse(Console.ReadLine());
+
+Console.Write("Введите максимальное значение элементов массива: ");
+int max = int.Parse(Console.ReadLine());
+
+int[,] CreateMatrixRndInt(int m, int n, int minimum, int maximum)
+{
+    int[,] arr = new int[m, n];
+    Random rnd = new Random();
+
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = rnd.Next(minimum, maximum + 1);
+        }
+    }
+    return arr;
+}
+
+double[] MatrixSumColumnlNumbers(int[,] inArray)
+{
+    double[] result = new double[inArray.GetLength(1)];
+    for (int j = 0; j < inArray.GetLength(1); j++)
+    {
+        double sum = 0;
+        double count = 0;
+        for (int i = 0; i < inArray.GetLength(0); i++)
+        {
+            sum = sum + inArray[i, j];
+            count++;
+        }
+        result[j] = Math.Round(sum / count, 2);
+    }
+    return result;
+}
+
+
+
+void PrintMatrix(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if (j == 0) Console.Write("|");
+            if (j < arr.GetLength(1) - 1) Console.Write($"{arr[i, j],3} |");
+            else Console.Write($"{arr[i, j],3} |");
+        }
+        Console.WriteLine();
+    }
+}
+
+void PrintArray(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i == 0) Console.Write("Среднее арифметическое каждого столбца: ");
+        if (i < array.Length - 1) Console.Write(array[i] + ",  ");
+        else Console.Write(array[i] + ".");
+    }
+    Console.WriteLine("\b");
+}
+
+int[,] arrayResult = CreateMatrixRndInt(m, n, min, max);
+PrintMatrix(arrayResult);
+Console.WriteLine();
+MatrixSumColumnlNumbers(arrayResult);
+double[] sumArrCol = MatrixSumColumnlNumbers(arrayResult);
+PrintArray(sumArrCol);
